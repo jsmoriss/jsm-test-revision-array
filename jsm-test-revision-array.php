b3dfe51d6885e4af2e6eccd8b9d9f200d8ad6aa5
JS Morisset <jsm@surniaulula.com>
2024-01-06 08:13:11 -0800

    v1.0.1

--- jsm-test-revision-array.php
+++ jsm-test-revision-array.php
@@ -11,7 +11,7 @@
  * License URI: https://www.gnu.org/licenses/gpl.txt
  * Description: A plugin to test WP revisions with arrays.
  * Requires PHP: 7.2.34
- * Requires At Least: 5.5
+ * Requires At Least: 5.8
  * Tested Up To: 6.4.2
  * Version: 1.0.1
  *
