125724130d52b81434af6f01902ddf579370d81d
jsmoriss <jsm@surniaulula.com>
2024-04-09 22:28:28 +0000

    v1.0.1

--- jsm-test-revision-array.php
+++ jsm-test-revision-array.php
@@ -12,7 +12,7 @@
  * Description: A plugin to test WP revisions with arrays.
  * Requires PHP: 7.2.34
  * Requires At Least: 5.8
- * Tested Up To: 6.5.0
+ * Tested Up To: 6.5.2
  * Version: 1.0.1
  *
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
